d5a2ca7d63b0c793f8e6bf1e68e563d4d6f82852
SKN <skn437physx@gmail.com>
2022-09-18 23:10:21 +0600

    Created Image Branch separately

---
+++ src/api/image-file/controllers/image-file.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * image-file controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::image-file.image-file');
---
+++ src/api/image-file/routes/image-file.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * image-file router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::image-file.image-file');
